33f72aaa388388fcd0d83abc5956f7e3ca5048f7
DodoMCanva <fdogasa@hotmail.com>
2025-09-18 12:33:44 -0700

    create projects

---
+++ Servidor/src/inicio/arranque.java
@@ -0,0 +1,17 @@
+
+package inicio;
+
+/**
+ *
+ * @author dodo
+ */
+public class arranque {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
